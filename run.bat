cb2c191f0eb38dd4a0b8560aa97b7fd33c5a1219
S0rrow <jae9hell@gmail.com>
2022-05-05 04:14:47 +0900

    adding LAS again as submodule

--- run.bat
+++ run.bat
@@ -1,1 +1,8 @@
+@ECHO OFF
+cd LAS
+rem mvn clean package
+
+java -cp ./LAS/target/LAS-0.0.1-SNAPSHOT.jar main.LAS
+rem  C:/repository/fv4202/pool/Closure_rank-1_old.java C:/repository/fv4202/pool/Closure_rank-1_new.java
+cd ..
 rem python ./main.py -g target/gumtreeVector.csv
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 
+java -cp ./LAS/target/LAS-0.0.1-SNAPSHOT.jar main.LAS ./pool/*_old.java ./pool*_new.java
 # python ./main.py -g target/gumtreeVector.csv
