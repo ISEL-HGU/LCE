b322e54a3a37f81a316ede490bc091ecc0fabaf9
S0rrow <jae9hell@gmail.com>
2022-05-05 06:42:13 +0900

    shell script modified

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-
-java -cp ./LAS/target/LAS-0.0.1-SNAPSHOT.jar main.LAS ./pool/*_old.java ./pool*_new.java
-# python ./main.py -g target/gumtreeVector.csv
+cd ./result
+del *.csv
+cd ..
+python ./main.py -g gumtreeVector.csv -t testVector.csv -r 60
