160bac8db3ea83eb144b0f2eff8f6bc0cdccd76a
S0rrow <jae9hell@gmail.com>
2022-05-12 23:37:14 +0900

    new python code for getting diff files from change vectors

---
+++ validator.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import csv
+import numpy as np
+import getopt
+
+def csv_to_array(csv_file):
+    f_csv_file = open(csv_file, 'r')
+    result_array = csv.reader(f_csv_file)
+
+    result_array = np.asarray(list(result_array))
+    return result_array
+
+def top_diff_to_text(vec):
+    return
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv[1:], "h:f:d:", ["help", "file", "directory"])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    file = ''
+    gitdir = ''
+    for o, a in opts:
+        if o in ("-H", "--help") or o in ("-h", "--hash"):
+            print("")
+            sys.exit()
+        elif o in ("-f", "--file"):
+            file = a
+        elif o in ("-d", "--directory"):
+            gitdir = a
+        else:
+            assert False, "unhandled option"
+
+    pool_dir = "./pool/"
+    result_dir = "./result/"
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
