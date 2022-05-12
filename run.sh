678ca9c3b4087fd3f91672a16158703bcda89424
S0rrow <jae9hell@gmail.com>
2022-05-12 21:06:29 +0900

    new change vector and metadata added

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd ./result
 rm *.csv
 cd ..
-python3 ./main.py -g gumtreeVector.csv -t testVector.csv -r 60
+# python3 ./main.py -g gumtreeVector.csv -t testVector.csv -r 60
+python3 ./main.py -g Grasscutter_vector.csv -c Grasscutter_file_commit.csv -t testVector.csv
